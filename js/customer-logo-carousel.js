12943a60440fbd17522698b41d7f4f3fc6379a9c
rahatcnits <rahatcnits@gmail.com>
2023-05-07 18:27:50 +0600

    54th commit

--- js/customer-logo-carousel.js
+++ js/customer-logo-carousel.js
@@ -14,9 +14,14 @@ $(document).ready(function () {
 
 
     responsive: [{
+      breakpoint: 990,
+      settings: {
+        slidesToShow: 3
+      }
+    }, {
       breakpoint: 768,
       settings: {
-        slidesToShow: 4
+        slidesToShow: 2
       }
     }, {
       breakpoint: 575,
