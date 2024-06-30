578af11629010f06ce62304f513655fb3def98f4
rahatcnits <rahatcnits@gmail.com>
2024-06-30 17:03:02 +0600

    hero completed

---
+++ js/slide_clone.js
@@ -0,0 +1,5 @@
+let copy1 = document.querySelector(".logos_slide_panel1").cloneNode(true);
+document.querySelector(".hero_slider1").appendChild(copy1);
+
+let copy2 = document.querySelector(".logos_slide_panel2").cloneNode(true);
+document.querySelector(".hero_slider2").appendChild(copy2);
